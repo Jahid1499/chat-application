ba7037a3a25395970db0bdddf990741b235a1e8e
jahid <jahid15-1499@diu.edu.bd>
2024-05-30 19:02:19 +0600

    conversation api file create, create getconversations, getMoreConversations, getConversation, editConverstaion api with install socket.io client and configure it

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedOut } from "../auth/authSlice";
 
+
 const baseQuery = fetchBaseQuery({
     baseUrl: import.meta.env.VITE_APP_URL,
     prepareHeaders: async (headers, { getState, endpoint }) => {
