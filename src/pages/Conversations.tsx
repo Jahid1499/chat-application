c77a45f151570391eea6ab81961e3bd8b11a2a66
jahid <jahid15-1499@diu.edu.bd>
2024-05-31 00:20:26 +0600

    conversations feature done

--- src/pages/Conversations.tsx
+++ src/pages/Conversations.tsx
@@ -1,5 +1,21 @@
-const Conversations = () => {
-  return <h1>Conversations</h1>;
-};
+import Blank from "../components/conversations/Blank";
+import Navigation from "../components/ui/Navigation";
+import Sidebar from "../components/ui/Sidebar";
 
-export default Conversations;
+export default function Inbox() {
+  return (
+    <div>
+      <Navigation />
+      <div className="max-w-7xl mx-auto -mt-1">
+        <div className="min-w-full border rounded flex lg:grid lg:grid-cols-3">
+          <Sidebar />
+          <div className="w-full lg:col-span-2 lg:block">
+            <div className="w-full grid conversation-row-grid">
+              <Blank />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
