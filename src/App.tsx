b255237d68df6b0e807b49351d7db3c12fde2d85
jahid <jahid15-1499@diu.edu.bd>
2024-05-30 20:05:47 +0600

    public and private route integrate in app.tsx file
    public and private route load depends on authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
-function App() {
-  return (
-    <>
-      <h1 className="bg-green-300 text-white px-3 py-3">Hello bangladesh</h1>
-    </>
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
+import useAuthCheck from "./hooks/useAuthCheck";
+import Conversations from "./pages/Conversations";
+import Inbox from "./pages/Inbox";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const App = () => {
+  const authChecked = useAuthCheck();
+  return !authChecked ? (
+    <div>Checking authentication....</div>
+  ) : (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/inbox"
+          element={
+            <PrivateRoute>
+              <Conversations />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/inbox/:id"
+          element={
+            <PrivateRoute>
+              <Inbox />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
